b662c0b09bf7204915c50739010cfad499bb3679
Ed Rands <erands@gmail.com>
2020-05-31 14:52:20 -0700

    chore(jest): add additional support packages

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  setupFilesAfterEnv: ["jest-extended", "expect-more-jest", "jest-chain"],
   coverageDirectory: "tests/reports/unit/coverage",
   errorOnDeprecated: true,
 }
