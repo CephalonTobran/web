fee94f9c76a600e6591f6a1d19153e75e475edce
Ed Rands <erands@gmail.com>
2020-06-05 09:21:17 -0700

    test(app): add suite for TheAppBar component

---
+++ tests/unit/components/TheAppBar.spec.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import TheAppBar from "@/components/TheAppBar.vue"
+import { shallowMount } from "@vue/test-utils"
+
+describe("TheAppBar component", () => {
+  it("should display the app name", () => {
+    const displayText = "Testing Cephalon Tobran"
+    const wrapper = shallowMount(TheAppBar, {
+      mocks: {
+        $t: () => displayText,
+      },
+    })
+    expect(wrapper.get("v-toolbar-title-stub").text()).toBe(displayText)
+  })
+
+  describe("menu toggle button", () => {
+    it("should exist", () => {
+      const wrapper = shallowMount(TheAppBar, {
+        mocks: {
+          $t: () => {},
+        },
+      })
+      expect(wrapper.get("v-app-bar-nav-icon-stub").exists()).toBeTrue()
+    })
+
+    it.todo("should toggle the main menu display when clicked")
+  })
+})
