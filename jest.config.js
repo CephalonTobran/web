c340735ae90a372609adf4a65beddd37ad20fe15
Ed Rands <erands@gmail.com>
2020-06-04 21:52:10 -0700

    test(jest): add setup for Vue and Vuetify

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  setupFilesAfterEnv: ["jest-extended", "expect-more-jest", "jest-chain"],
+  setupFilesAfterEnv: [
+    "jest-extended",
+    "expect-more-jest",
+    "jest-chain",
+    "<rootDir>/tests/unit/setup/vue.ts",
+  ],
   coverageDirectory: "tests/reports/unit/coverage",
   errorOnDeprecated: true,
   notify: true,
