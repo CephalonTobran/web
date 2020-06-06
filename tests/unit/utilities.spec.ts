1381f67c1f92fed85b7d65892444d18f2e439ca6
Ed Rands <erands@gmail.com>
2020-06-06 11:43:05 -0700

    test(app): remove a pointless test

--- tests/unit/utilities.spec.ts
+++ tests/unit/utilities.spec.ts
@@ -6,11 +6,6 @@ describe("percentage", () => {
     expect(testResult).toBe(20)
   })
 
-  it("returns an integer", () => {
-    const testResult = percentage(11, 87)
-    expect(testResult).toBe(12)
-  })
-
   it("returns 0 when total is less than 1", () => {
     const testResult = percentage(10, -1)
     expect(testResult).toBe(0)
