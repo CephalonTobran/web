4c8df0506eab9c56098877c86ca9e361d1f8faad
Ed Rands <erands@gmail.com>
2020-06-06 12:06:51 -0700

    test(app): refactor suite for TheApp to avoid console errors about <router-view>

--- tests/unit/components/TheApp.spec.ts
+++ tests/unit/components/TheApp.spec.ts
@@ -5,33 +5,31 @@ import TheScrollToTopButton from "@/components/TheScrollToTopButton.vue"
 import { shallowMount } from "@vue/test-utils"
 
 describe("TheApp component", () => {
-  it("should load TheMainMenu component once", () => {
-    const wrapper = shallowMount(TheApp)
+  describe("for unique child components", () => {
+    const wrapper = shallowMount(TheApp, { stubs: { "router-view": true } })
 
-    const allTheMainMenus = wrapper.findAllComponents(TheMainMenu)
-    expect(allTheMainMenus).toHaveLength(1)
+    it("should load TheMainMenu component once", () => {
+      const allTheMainMenus = wrapper.findAllComponents(TheMainMenu)
+      expect(allTheMainMenus).toHaveLength(1)
 
-    const theMainMenu = wrapper.findAllComponents(TheMainMenu).at(0)
-    expect(theMainMenu.exists()).toBeTrue()
-  })
+      const theMainMenu = wrapper.findAllComponents(TheMainMenu).at(0)
+      expect(theMainMenu.exists()).toBeTrue()
+    })
 
-  it("should load TheAppBar component once", () => {
-    const wrapper = shallowMount(TheApp)
+    it("should load TheAppBar component once", () => {
+      const allTheAppBars = wrapper.findAllComponents(TheAppBar)
+      expect(allTheAppBars).toHaveLength(1)
 
-    const allTheAppBars = wrapper.findAllComponents(TheAppBar)
-    expect(allTheAppBars).toHaveLength(1)
+      const theAppBar = wrapper.findAllComponents(TheAppBar).at(0)
+      expect(theAppBar.exists()).toBeTrue()
+    })
 
-    const theAppBar = wrapper.findAllComponents(TheAppBar).at(0)
-    expect(theAppBar.exists()).toBeTrue()
-  })
+    it("should load TheScrollToTopButton component once", () => {
+      const allTheScrollToTopButtons = wrapper.findAllComponents(TheScrollToTopButton)
+      expect(allTheScrollToTopButtons).toHaveLength(1)
 
-  it("should load TheScrollToTopButton component once", () => {
-    const wrapper = shallowMount(TheApp)
-
-    const allTheScrollToTopButtons = wrapper.findAllComponents(TheScrollToTopButton)
-    expect(allTheScrollToTopButtons).toHaveLength(1)
-
-    const theScrollToTopButton = wrapper.findAllComponents(TheScrollToTopButton).at(0)
-    expect(theScrollToTopButton.exists()).toBeTrue()
+      const theScrollToTopButton = wrapper.findAllComponents(TheScrollToTopButton).at(0)
+      expect(theScrollToTopButton.exists()).toBeTrue()
+    })
   })
 })
