07b790d363fccc561baeeb26ef47bb179720e7a0
Ed Rands <erands@gmail.com>
2020-06-13 10:57:44 -0700

    test(app): add placeholder suites for plugin loaders

---
+++ tests/unit/plugins/i18n.spec.ts
@@ -0,0 +1,7 @@
+import i18n from "@/plugins/i18n"
+import VueI18n from "vue-i18n"
+
+describe("i18n plugin", () => {
+  // require.context() is breaking the ability to test this
+  it.todo("should return a VueI18n object")
+})
---
+++ tests/unit/plugins/vuetify.spec.ts
@@ -0,0 +1,6 @@
+import vuetify from "@/plugins/vuetify"
+import Vuetify from "vuetify/lib"
+
+describe("vuetify plugin", () => {
+  it.todo("should return a Vuetify object")
+})
