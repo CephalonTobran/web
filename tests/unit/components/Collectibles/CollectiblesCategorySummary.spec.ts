3e9b7fe4dc61af36f02d10e73555126a623b11c0
Ed Rands <erands@gmail.com>
2020-06-13 10:07:40 -0700

    test(app): create a placeholder suite for CollectiblesCategorySummary component
    I'm not sure how to handle the require() of the image

---
+++ tests/unit/components/Collectibles/CollectiblesCategorySummary.spec.ts
@@ -0,0 +1,7 @@
+import CollectiblesCategorySummary from "@/components/Collectibles/CollectiblesCategorySummary.vue"
+import { mount } from "@vue/test-utils"
+
+describe("CollectiblesCategorySummary component", () => {
+  // TODO: figure out how to deal with the require() for the image
+  it.todo("should display the category name")
+})
